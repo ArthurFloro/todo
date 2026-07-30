3118f50260ed318e1ecee21f2da35e735f0ed89a
ArthurFloro <arthur.floro2006@gmail.com>
2026-07-30 19:42:59 -0300

    feat(ui): cria componente Input Text customizado
    - Cria o arquivo  e estrutura as propriedades do componente.
    - Utiliza a biblioteca  para definir os estilos base, incluindo fundo transparente e borda rosa no estado de foco ().
    - Implementa variações de tamanho e o estado , bloqueando interações quando necessário.
    - Aplica a função utilitária  para combinar as classes do Tailwind de forma dinâmica e segura.

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,11 @@ import Icon from "./components/icon";
 import Badge from "./components/badge";
 import Button from "./components/button";
 import ButtonIcon from "./components/button-icon";
+import InputText from "./components/input-text";
 
 export default function App() {
   return (
-    <div className="grid gap-3">
+    <div className="grid gap-10">
       <div className="flex flex-col gap-2">
         <Text as="text" variant="body-sm-bold" className="text-pink-base"> Olá mundo </Text>
         <Text as="text" variant="body-sm-bold" className="text-green-base"> Olá mundo </Text>
@@ -42,6 +43,10 @@ export default function App() {
         <ButtonIcon icon={TrashIcon} />
         <ButtonIcon icon={TrashIcon} variant={"secondary"} />
         <ButtonIcon icon={TrashIcon} variant={"tertiary"} />
+      </div>
+
+      <div>
+        <InputText />
       </div>
     </div>
   )

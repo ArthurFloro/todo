ec921b6b479e94249bfda404a2bf4aaf59416c02
ArthurFloro <arthur.floro2006@gmail.com>
2026-07-30 13:31:45 -0300

    feat(ui): cria componente de ícone dinâmico e configura SVGR
    - Configura o plugin SVGR no Vite para transformar arquivos SVG nativos em componentes React.
    - Cria um componente base de ícone flexível e reutilizável.
    - Adiciona suporte a propriedades customizadas, permitindo controle de tamanhos e animações.
    - Facilita a aplicação direta de classes do Tailwind para estilização dinâmica dos SVGs.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import Text from "./components/text";
+import TrashIcon from './assets/icons/trash.svg?react'
+import CheckIcon from "./assets/icons/check.svg?react";
+import XIcon from "./assets/icons/x.svg?react";
+import PencilIcon from "./assets/icons/pencil.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import Icon from "./components/icon";
 
 export default function App() {
   return (
-    <div className="flex flex-col gap-2">
-      <Text as="text" variant="body-sm-bold" className="text-pink-base"> Olá mundo </Text>
-      <Text as="text" variant="body-sm-bold" className="text-green-base"> Olá mundo </Text>
-      <Text as="text" variant="body-sm-bold" className="text-gray-400"> Olá mundo </Text>
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-2">
+        <Text as="text" variant="body-sm-bold" className="text-pink-base"> Olá mundo </Text>
+        <Text as="text" variant="body-sm-bold" className="text-green-base"> Olá mundo </Text>
+        <Text as="text" variant="body-sm-bold" className="text-gray-400"> Olá mundo </Text>
 
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-green-base" />
+        <Icon svg={CheckIcon} />
+        <Icon svg={PlusIcon} />
+        <Icon svg={SpinnerIcon} />
+        <Icon svg={PencilIcon} />
+        <Icon svg={XIcon} />
+      </div>
     </div>
   )
 }
---
+++ src/components/icon.tsx
@@ -0,0 +1,13 @@
+import type { VariantProps } from "class-variance-authority";
+import type React from "react";
+import { iconVariants } from "../utils/icon-variants";
+
+interface IconProps extends React.ComponentProps<"svg">, VariantProps<typeof iconVariants> {
+  svg: React.FC<React.ComponentProps<'svg'>>
+}
+
+export default function Icon({ svg: SvgComponent, animate, className, ...props }: IconProps) {
+  return (
+    <SvgComponent className={iconVariants({ animate, className })} {...props} />
+  )
+}

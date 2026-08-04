d5bdd41f059fcb1599fa9e1b8886265ea16c82f5
ArthurFloro <arthur.floro2006@gmail.com>
2026-08-04 13:12:32 -0300

    feat(layout): cria listagem de tarefas e finaliza estrutura da interface
    - Implementa o componente Tasks List (	asks-list.tsx) para agrupar os itens da aplicação.
    - Integra o botão de ´Nova Tarefa´ diretamente na lista, adotando uma abordagem pragmática para evitar componentização excessiva.
    - Realiza ajustes de CSS/Tailwind para garantir a responsividade e o funcionamento fluido em dispositivos móveis e desktop.
    - Finaliza a estrutura visual principal, deixando a aplicação pronta para receber as regras de negócio.

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,5 +1,5 @@
 import Container from "../components/container";
-import TaskItem from "../core-components/task-item";
+import TasksList from "../core-components/tasks-lists";
 import TasksSummary from "../core-components/tasks-summary";
 
 export default function PageHome() {
@@ -8,6 +8,7 @@ export default function PageHome() {
       <TasksSummary />
     </header>
 
-    <TaskItem />
+
+    <TasksList />
   </Container>
 }

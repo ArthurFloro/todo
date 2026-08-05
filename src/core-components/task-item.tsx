f5a585e687aff2150fa471720410369e983eb25b
ArthurFloro <arthur.floro2006@gmail.com>
2026-08-05 08:42:07 -0300

    feat(tasks): implementa listagem dinâmica e tipagem segura das propriedades
    - Substitui elementos fixos na interface pelo mapeamento dinâmico das tarefas salvas no local storage.
    - Adiciona tipagem estática através de interfaces para as propriedades dos componentes de tarefa.
    - Implementa tratamentos de segurança para evitar quebras na aplicação ao lidar com valores ausentes ou nulos.
    - Prepara a estrutura de estados dinâmicos para a futura coleta de dados do formulário e atualização de status.

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -8,9 +8,15 @@ import XIcon from "../assets/icons/x.svg?react"
 import CheckIcon from "../assets/icons/check.svg?react"
 import { useState } from "react";
 import InputText from "../components/input-text";
+import { TaskState, type Task } from "../models/task";
+import { cx } from "class-variance-authority";
 
-export default function TaskItem() {
-  const [isEditing, setIsEditing] = useState(false)
+interface TaskItemProps {
+  task: Task
+}
+
+export default function TaskItem({ task }: TaskItemProps) {
+  const [isEditing, setIsEditing] = useState(task?.state === TaskState.Creating)
 
   function handleEditTask() {
     setIsEditing(true)
@@ -24,8 +30,10 @@ export default function TaskItem() {
     {!isEditing ?
       (
         <>
-          <InputCheckbox />
-          <Text className="flex-1">Fazer compras da semana</Text>
+          <InputCheckbox value={task?.concluded?.toString()} checked={task?.concluded} />
+          <Text className={cx("flex-1", {
+            'line-through': task?.concluded
+          })}>{task?.title}</Text>
           <div className="flex gap-1">
             <ButtonIcon icon={TrashIcon} variant={"tertiary"} />
             <ButtonIcon icon={PencilIcon} variant={"tertiary"} onClick={handleEditTask} />

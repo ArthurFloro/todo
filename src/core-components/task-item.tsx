516d1cbd5cab92785f700aaa1a7387f0d8fef234
ArthurFloro <arthur.floro2006@gmail.com>
2026-08-05 13:28:48 -0300

    feat(tasks): implementa exclusão de tarefas e finaliza CRUD
    - Cria a função handleClickDeleteTask que aciona deleteTask para o manuseio da remoção de itens.
    - Utiliza o método ilter para remover a tarefa correspondente ao ID, refletindo a exclusão imediatamente na interface e no localStorage.
    - Implementa o tratamento de estado para garantir que tarefas excluídas durante o modo de criação ou edição sejam completamente descartadas.
    - Conclui as operações básicas de CRUD do sistema de gerenciamento de tarefas.

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -19,13 +19,17 @@ interface TaskItemProps {
 export default function TaskItem({ task }: TaskItemProps) {
   const [isEditing, setIsEditing] = useState(task?.state === TaskState.Creating)
   const [taskTitle, setTaskTitle] = useState(task.title || "")
-  const { updateTask, updateTaskStatus } = useTask()
+  const { updateTask, updateTaskStatus, deleteTask } = useTask()
 
   function handleEditTask() {
     setIsEditing(true)
   }
 
   function handleExitEditTask() {
+    if (task.state === TaskState.Creating) {
+      deleteTask(task.id)
+    }
+
     setIsEditing(false)
   }
 
@@ -46,6 +50,10 @@ export default function TaskItem({ task }: TaskItemProps) {
     updateTaskStatus(task.id, checked)
   }
 
+  function handleDeleteTask() {
+    deleteTask(task.id)
+  }
+
   return (
 
     <Card size={"md"} >
@@ -60,7 +68,7 @@ export default function TaskItem({ task }: TaskItemProps) {
               'line-through': task?.concluded
             })}>{task?.title}</Text>
             <div className="flex gap-1">
-              <ButtonIcon type="button" icon={TrashIcon} variant={"tertiary"} />
+              <ButtonIcon type="button" icon={TrashIcon} variant={"tertiary"} onClick={handleDeleteTask} />
               <ButtonIcon type="button" icon={PencilIcon} variant={"tertiary"} onClick={handleEditTask} />
             </div>
           </div>

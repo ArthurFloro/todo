c7e96485a91ef557ae4096b6c602da01a0434a55
ArthurFloro <arthur.floro2006@gmail.com>
2026-08-04 22:54:23 -0300

    feat(hooks): implementa função prepareTask no gerenciamento de tarefas
    - Adiciona a função  dentro do hook  para inicializar o fluxo de criação.
    - Gera um ID aleatório e define o estado inicial da nova tarefa como creating antes do salvamento definitivo.
    - Utiliza a integração com  para gerenciar a renderização temporária do card de criação.
    - Prepara o estado da aplicação para as próximas etapas de listagem e atualização de tarefas.

---
+++ src/hooks/use-task.ts
@@ -0,0 +1,22 @@
+import useLocalStorage from "use-local-storage-state"
+import { TASK_KEY, TaskState, type Task } from "../models/task"
+
+export default function useTask() {
+    const [tasks, setTasks] = useLocalStorage<Task[]>(TASK_KEY, {
+        defaultValue: []
+    })
+
+    function prepareTask() {
+        setTasks([...tasks, {
+            id: Math.random().toString(36).substring(2, 9),
+            title: "",
+            state: TaskState.Creating
+        }])
+    }
+
+    return {
+        prepareTask
+    }
+
+
+}
